f555a3649e7341d5807dbd8a07385a2f2d252672
Matix Media, Inc <47772205+Matix-Media@users.noreply.github.com>
2020-02-08 00:08:39 +0100

    Fist and last upload

---
+++ ArchivDecompiler/Form1.Designer.vb
@@ -0,0 +1,254 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim ListViewItem1 As System.Windows.Forms.ListViewItem = New System.Windows.Forms.ListViewItem(New String() {"Einstellungen", "..."}, -1)
+        Dim ListViewItem2 As System.Windows.Forms.ListViewItem = New System.Windows.Forms.ListViewItem(New String() {"Entpacken", "..."}, -1)
+        Dim ListViewItem3 As System.Windows.Forms.ListViewItem = New System.Windows.Forms.ListViewItem(New String() {"Fertig", "..."}, -1)
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.tbFile = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.tbdestination = New System.Windows.Forms.TextBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.lvStatus = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.lvErrors = New System.Windows.Forms.ListView()
+        Me.ColumnHeader5 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.tbFile)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(433, 52)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Datei"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(360, 18)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(67, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Öffnen..."
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'tbFile
+        '
+        Me.tbFile.Location = New System.Drawing.Point(6, 19)
+        Me.tbFile.Name = "tbFile"
+        Me.tbFile.Size = New System.Drawing.Size(348, 20)
+        Me.tbFile.TabIndex = 0
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.CheckBox1)
+        Me.GroupBox2.Controls.Add(Me.Button2)
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Controls.Add(Me.tbdestination)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 70)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(433, 86)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Entpacken"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(6, 58)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(142, 17)
+        Me.CheckBox1.TabIndex = 3
+        Me.CheckBox1.Text = "Nach Entpacken Öffnen"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(360, 31)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(67, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Öffnen..."
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(31, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "nach"
+        '
+        'tbdestination
+        '
+        Me.tbdestination.Location = New System.Drawing.Point(6, 32)
+        Me.tbdestination.Name = "tbdestination"
+        Me.tbdestination.Size = New System.Drawing.Size(348, 20)
+        Me.tbdestination.TabIndex = 0
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.lvStatus)
+        Me.GroupBox3.Location = New System.Drawing.Point(12, 217)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(433, 107)
+        Me.GroupBox3.TabIndex = 2
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Status"
+        '
+        'lvStatus
+        '
+        Me.lvStatus.BackColor = System.Drawing.SystemColors.Control
+        Me.lvStatus.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.lvStatus.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2})
+        Me.lvStatus.FullRowSelect = True
+        Me.lvStatus.Items.AddRange(New System.Windows.Forms.ListViewItem() {ListViewItem1, ListViewItem2, ListViewItem3})
+        Me.lvStatus.Location = New System.Drawing.Point(6, 19)
+        Me.lvStatus.Name = "lvStatus"
+        Me.lvStatus.Size = New System.Drawing.Size(421, 82)
+        Me.lvStatus.TabIndex = 0
+        Me.lvStatus.UseCompatibleStateImageBehavior = False
+        Me.lvStatus.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "Eigenschaft"
+        Me.ColumnHeader1.Width = 114
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Status"
+        Me.ColumnHeader2.Width = 302
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.lvErrors)
+        Me.GroupBox4.Location = New System.Drawing.Point(12, 330)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(433, 105)
+        Me.GroupBox4.TabIndex = 3
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Fehler"
+        '
+        'lvErrors
+        '
+        Me.lvErrors.BackColor = System.Drawing.SystemColors.Control
+        Me.lvErrors.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.lvErrors.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader5})
+        Me.lvErrors.FullRowSelect = True
+        Me.lvErrors.Location = New System.Drawing.Point(6, 19)
+        Me.lvErrors.Name = "lvErrors"
+        Me.lvErrors.Size = New System.Drawing.Size(421, 80)
+        Me.lvErrors.TabIndex = 1
+        Me.lvErrors.UseCompatibleStateImageBehavior = False
+        Me.lvErrors.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader5
+        '
+        Me.ColumnHeader5.Text = "Fehler"
+        Me.ColumnHeader5.Width = 400
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(12, 162)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(433, 23)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Entpacken"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 197)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(433, 11)
+        Me.ProgressBar1.TabIndex = 5
+        Me.ProgressBar1.Visible = False
+        '
+        'BackgroundWorker1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(457, 443)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.GroupBox4)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Archiv Entpacken"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox4.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents tbFile As TextBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents tbdestination As TextBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents lvStatus As ListView
+    Friend WithEvents ColumnHeader1 As ColumnHeader
+    Friend WithEvents ColumnHeader2 As ColumnHeader
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents lvErrors As ListView
+    Friend WithEvents ColumnHeader5 As ColumnHeader
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents CheckBox1 As CheckBox
+End Class
---
+++ ArchivDecompiler/Form1.vb
@@ -0,0 +1,210 @@
+﻿Imports System.ComponentModel
+Imports System.IO, Ionic.Zip
+
+Public Class Form1
+    Dim file_ As String
+    Dim destination As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim parameter() As String = Environment.GetCommandLineArgs().ToArray
+        If parameter.Count - 1 >= 1 Then 'Höher als 1 weil der index 0 der Pfad zum programm ist
+            For i = 1 To parameter.Count - 1
+                Select Case i
+                    Case 1
+                        file_ = parameter(i)
+                    Case 2
+                        destination = parameter(i)
+                End Select
+            Next
+        End If
+    End Sub
+
+    Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        tbFile.Text = file_
+        If destination = "" AndAlso Not file_ = "" Then
+            destination = Path.GetDirectoryName(file_) & "\" & Path.GetFileNameWithoutExtension(file_)
+            tbdestination.Text = destination
+        ElseIf Not destination = "" Then
+            tbdestination.Text = destination
+        ElseIf destination = "" Then
+            tbdestination.Text = ""
+        End If
+
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim bfd As New OpenFileDialog
+        bfd.Filter = "ZIP-Archiv|*.zip"
+        bfd.Multiselect = False
+        If bfd.ShowDialog = DialogResult.OK Then
+            If File.Exists(bfd.FileName) Then
+                tbFile.Text = bfd.FileName
+                file_ = bfd.FileName
+                If tbdestination.Text = "" Then
+                    tbdestination.Text = Path.GetDirectoryName(bfd.FileName)
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim fbd As New FolderBrowserDialog
+        If fbd.ShowDialog = DialogResult.OK Then
+            Dim ff As String = fbd.SelectedPath
+            If File.Exists(ff) Then
+                tbdestination.Text = ff
+                destination = ff
+            End If
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' wartet x Millisekunden (GUI-freundlich)
+    ''' </summary>
+    Public Sub WaitOnGUI(ByVal ms As Integer)
+        ' timeout festlegen
+        Dim timeOut As Date = Date.Now().AddMilliseconds(ms)
+        ' bis timeout warten
+        While timeOut > Date.Now
+            Application.DoEvents() ' GUI Ereignisse erlauben
+        End While
+    End Sub
+
+    Private TotalSize As ULong = 0
+
+    Private ExtractedSize As ULong = 0
+
+    Private LastBtsTransfered As ULong = 0
+
+    Private BufferSize As ULong = 0
+
+    Dim oa As Boolean = False
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        lvStatus.Items(0).SubItems(1).Text = "..."
+        lvStatus.Items(1).SubItems(1).Text = "..."
+        lvStatus.Items(2).SubItems(1).Text = "..."
+        If File.Exists(tbFile.Text) AndAlso Directory.Exists(Path.GetDirectoryName(tbdestination.Text)) Then
+            lvStatus.Items(0).SubItems(1).Text = "Abgeschlossen."
+            GroupBox1.Enabled = False
+            GroupBox2.Enabled = False
+            Button3.Enabled = False
+            ProgressBar1.Visible = True
+            ProgressBar1.Value = 0
+            If CheckBox1.Checked = True Then
+                oa = True
+            End If
+            WaitOnGUI(100)
+            BackgroundWorker1.RunWorkerAsync()
+        Else
+
+            With lvErrors.Items.Add("Der/Die angegebene/n Pfad/e ist/sind Falsch.")
+                .ToolTipText = "Der/Die angegebene/n Pfad/e ist/sind Falsch."
+            End With
+        End If
+
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+
+        Try
+            Dim ZipToUnpack As String = tbFile.Text
+            Dim TargetDir As String = tbdestination.Text
+            BeginInvoke(Sub() lvStatus.Items(1).SubItems(1).Text = "Extrahiere Archiv """ & ZipToUnpack & """ zu """ & TargetDir & """")
+            Using zip1 As ZipFile = ZipFile.Read(ZipToUnpack)
+                AddHandler zip1.ExtractProgress, AddressOf MyExtractProgress
+
+                'reset these values each time you unzip a folder
+                BufferSize = CULng(zip1.BufferSize)
+                TotalSize = 0
+                ExtractedSize = 0
+
+                'get the total size in bytes to be extracted
+                For Each ent As ZipEntry In zip1
+                    TotalSize += CULng(ent.UncompressedSize)
+                Next
+
+                'if the total size is greater than Integer.MaxValue then divide it down by 1024 before setting the ProgressBar.Maximum
+                If TotalSize >= Integer.MaxValue Then
+                    BeginInvoke(Sub() ProgressBar1.Maximum = CInt(TotalSize / 1024))
+                Else
+                    BeginInvoke(Sub() ProgressBar1.Maximum = CInt(TotalSize))
+                End If
+
+                Dim e_ As ZipEntry
+                ' here, we extract every entry, but we could extract    
+                ' based on entry name, size, date, etc.   
+                For Each e_ In zip1
+                    BeginInvoke(Sub() lvStatus.Items(1).SubItems(1).Text = "Extrahiere Datei """ & e_.FileName & """...")
+                    BeginInvoke(Sub() lvStatus.Items(1).ToolTipText = "Extrahiere Datei """ & e_.FileName & """...")
+
+                    If File.Exists(TargetDir & "\" & e_.FileName.Replace("/", "\")) Then
+                        If MsgBox("Wollen Sie folgende Datei Überschreiben? """ & TargetDir & "\" & e_.FileName.Replace("/", "\") & """", MsgBoxStyle.YesNo, "Überschreiben") = MsgBoxResult.Yes Then
+                            e_.Extract(TargetDir, ExtractExistingFileAction.OverwriteSilently)
+                        Else
+
+                        End If
+                    Else
+                        e_.Extract(TargetDir, ExtractExistingFileAction.DoNotOverwrite)
+                    End If
+
+                Next
+            End Using
+            BeginInvoke(Sub() lvStatus.Items(1).SubItems(1).Text = "Abgeschlossen.")
+            BeginInvoke(Sub() lvStatus.Items(2).SubItems(1).Text = """" & ZipToUnpack & """ Erfogreich Extrahiert.")
+
+            BeginInvoke(Sub() GroupBox1.Enabled = True)
+            BeginInvoke(Sub() GroupBox2.Enabled = True)
+            BeginInvoke(Sub() Button3.Enabled = True)
+
+            BeginInvoke(Sub() lvErrors.Items.Clear())
+        Catch ex As Exception
+            Invoke(Sub() lvErrors.Items.Add(ex.Message))
+
+            BeginInvoke(Sub() GroupBox1.Enabled = True)
+            BeginInvoke(Sub() GroupBox2.Enabled = True)
+            BeginInvoke(Sub() Button3.Enabled = True)
+
+            BeginInvoke(Sub() lvStatus.Items(0).SubItems(1).Text = "Fehler!")
+            BeginInvoke(Sub() lvStatus.Items(1).SubItems(1).Text = "Fehler!")
+            BeginInvoke(Sub() lvStatus.Items(2).SubItems(1).Text = "Fehler!")
+
+        End Try
+    End Sub
+
+
+    Async Sub MyExtractProgress(ByVal sender As Object, ByVal e As ExtractProgressEventArgs)
+        'if the reported BytesTransferred is greater than 0 then add the number of bytes that where transferred to the ExtractedSize variable
+        'and set the LastBtsTransfered to the number of bytes that have been transferred so far. Else reset the LastBtsTransfered to 0.
+        If e.BytesTransferred > 0 Then
+            ExtractedSize += CULng(e.BytesTransferred - LastBtsTransfered)
+            LastBtsTransfered = CULng(e.BytesTransferred)
+        Else
+            LastBtsTransfered = 0
+        End If
+
+        'again, if the total size is greater than Integer.MaxValue then divide the ExtractedSize by 1024 before setting the ProgressBar.Value
+        If TotalSize >= Integer.MaxValue Then
+            BeginInvoke(Sub() ProgressBar1.Value = CInt(ExtractedSize / 1024))
+
+        Else
+            BeginInvoke(Sub() ProgressBar1.Value = CInt(ExtractedSize))
+        End If
+    End Sub
+
+    Private Sub BackgroundWorker1_RunWorkerCompleted(sender As Object, e As RunWorkerCompletedEventArgs) Handles BackgroundWorker1.RunWorkerCompleted
+        Try
+            If oa = True Then
+                Dim porc As New ProcessStartInfo
+                porc.Arguments = tbdestination.Text
+                porc.FileName = Application.StartupPath & "\" & "AddWExplorer.exe"
+                Process.Start(porc)
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        ProgressBar1.Visible = False
+    End Sub
+End Class
+
